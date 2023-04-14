e2a8a34ecc2366935626b124cc2fbe51eedbbbd4
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2023-04-14 08:37:02 +0530

    ytrty

--- Array.js
+++ Array.js
@@ -20,3 +20,32 @@ console.log(typeof(cars))
 console.log(Array.isArray(cars))
 console.log(cars.length)
 console.log(cars.sort())
+
+
+const myArray = [1, 2, 3, "four", true];
+
+myArray.push("five");
+console.log(myArray); // Output: [1, 2, 3, "four", true, "five"]
+myArray.pop();
+console.log(myArray); // Output: [1, 2, 3, "four", true]
+
+
+myArray.unshift("zero", -1);
+console.log(myArray); // Output: ["zero", -1, 1, 2, 3, "four", true]
+myArray.shift();
+console.log(myArray); // Output: [-1, 1, 2, 3, "four", true]
+
+
+
+const newArray = myArray.concat([6, 7, 8]);
+console.log(newArray); // Output: [-1, 1, 2, 3, "four", true, 6, 7, 8]
+
+
+
+// array.splice(startIndex, deleteCount, item1, item2, ...)
+// array.slice(startIndex, endIndex)
+
+const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+console.log(fruits)
+
+console.log(fruits.splice(1,3))
