97b1a2cf74db6a4d2b1e59dfd8de528b95430cd5
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2023-04-14 08:02:33 +0530

    df

---
+++ Array.js
@@ -0,0 +1,11 @@
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars)
+
+const cars = [];
+cars[0] = "Saab";
+cars[1] = "Volvo";
+cars[2] = "BMW";
+console.log(cars)
+const cars_new = new Array("Saab", "Volvo", "BMW");
+console.log(cars_new)   
