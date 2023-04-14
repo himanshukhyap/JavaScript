ab3a83ffb47d0400408d8bcef0d2fdb98976cb6a
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2023-04-14 09:10:53 +0530

    dfsd

--- Array.js
+++ Array.js
@@ -1,3 +1,41 @@
+// Here is a list of all the Array methods in JavaScript:
+
+// concat() - Joins two or more arrays and returns a new array.
+// filter() - Creates a new array with all elements that pass a test.
+// find() - Returns the value of the first element in an array that passes a test.
+// findIndex() - Returns the index of the first element in an array that passes a test.
+// flat() - Creates a new array with all sub-array elements concatenated into it recursively up to a specified depth.
+// flatMap() - Creates a new array by applying a function to each element of an array, then flattening the result into a new array.
+// forEach() - Executes a provided function once for each array element.
+// from() - Creates a new array from an array-like object.
+// includes() - Determines whether an array includes a certain value among its entries.
+// indexOf() - Returns the first index at which a given element can be found in the array, or -1 if it is not present.
+// join() - Joins all elements of an array into a string.
+// keys() - Returns an iterator object that contains the keys for each index in the array.
+// lastIndexOf() - Returns the last index at which a given element can be found in the array, or -1 if it is not present.
+// map() - Creates a new array with the results of calling a provided function on every element in the array.
+// pop() - Removes the last element from an array and returns that element.
+// push() - Adds one or more elements to the end of an array and returns the new length of the array.
+// reduce() - Applies a function to each element of an array and accumulates the result.
+// reduceRight() - Applies a function to each element of an array from right to left and accumulates the result.
+// reverse() - Reverses the order of the elements in an array.
+// shift() - Removes the first element from an array and returns that element.
+// slice() - Returns a shallow copy of a portion of an array into a new array object.
+// some() - Checks if at least one element in an array passes a test.
+// sort() - Sorts the elements of an array in place.
+// splice() - Adds or removes elements from an array at a specified index.
+// toLocaleString() - Returns a string representing the elements of the array, localized according to the browser's locale.
+// toString() - Returns a string representing the elements of the array.
+// unshift() - Adds one or more elements to the beginning of an array and returns the new length of the array.
+// values() - Returns an iterator object that contains the values for each index in the array.
+// Note that some of these methods may modify the original array, while others return a new array.
+
+
+
+
+
+
+
 
 const cars = ["Saab", "Volvo", "BMW"];
 console.log(cars)
@@ -45,7 +83,85 @@ console.log(newArray); // Output: [-1, 1, 2, 3, "four", true, 6, 7, 8]
 // array.splice(startIndex, deleteCount, item1, item2, ...)
 // array.slice(startIndex, endIndex)
 
-const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
-console.log(fruits)
+const fruits1 = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+const fruits2 = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
 
-console.log(fruits.splice(1,3))
+// If we want to create a new array that contains the elements from index 1 ('banana') to index 3 ('date'), we can use the slice method like this:
+
+const slicedFruits = fruits1.slice(1, 4);
+console.log(slicedFruits); // Output: ['banana', 'cherry', 'date']
+console.log(fruits1); // Output: ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+
+
+// If we want to remove the element at index 2 ('cherry') and add the element 'fig' at that same position, we can use the splice method like this:
+
+const removedElements = fruits2.splice(2, 1, 'fig');
+console.log(fruits2); // Output: ['apple', 'banana', 'fig', 'date', 'elderberry']
+console.log(removedElements); // Output: ['cherry']
+
+
+
+
+console.log(myArray.indexOf("four")); // Output: 4
+
+
+const filteredArray = myArray.filter((x) => typeof x === "number");
+console.log(filteredArray); // Output: [-1, 1, 2, 3]
+
+
+
+console.log(...myArray)
+
+
+// Destructuring an Array
+const [first, second, ...rest] = myArray;
+console.log(first); // Output: -1
+console.log(second); // Output: 1
+console.log(rest); // Output: [2, 3, "four", true]
+
+
+
+
+
+// The reduce() method in JavaScript is used to apply a function to each element in an array 
+// and accumulate a single value by iterating through the array. 
+// The reduce() method takes two arguments: a callback function and an optional initial value.
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((accumulator, currentValue) => {
+    console.log(accumulator)
+    console.log(currentValue)
+  return accumulator + currentValue;
+            // 1+2
+            // 3+3
+
+});
+
+console.log(sum); // Output: 15
+
+
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+const sum1 = numbers.reduce((accumulator, currentValue) => {
+    console.log(accumulator)
+    console.log(currentValue)
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum1); // Output: 15
+
+
+
+const people = [
+    { name: 'John', age: 20 },
+    { name: 'Sarah', age: 25 },
+    { name: 'Mike', age: 30 },
+    { name: 'Mary', age: 35 },
+  ];
+  const result = people.find(person => person.age >= 30);
+
+  console.log(result); // Output: { name: 'Mike', age: 30 }
+    
