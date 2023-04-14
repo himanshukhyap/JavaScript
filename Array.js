a59e9baa0de1a7e3073ed7dfa2a48e7f93ac7d82
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2023-04-14 08:10:35 +0530

    fgfdg

--- Array.js
+++ Array.js
@@ -1,13 +1,22 @@
 
-// const cars = ["Saab", "Volvo", "BMW"];
-// console.log(cars)
-
-const cars = [];
-cars[0] = "Saab";
-cars[1] = "Volvo";
-cars[2] = "BMW";
+const cars = ["Saab", "Volvo", "BMW"];
 console.log(cars)
+
+// const cars = [];
+// cars[0] = "Saab";
+// cars[1] = "Volvo";
+// cars[2] = "BMW";
+// console.log(cars)
 
 
 const cars_new = new Array("Saab", "Volvo", "BMW");
 console.log(cars_new)   
+
+cars[0] = "Opel";
+console.log(cars)
+
+
+console.log(typeof(cars))
+console.log(Array.isArray(cars))
+console.log(cars.length)
+console.log(cars.sort())
